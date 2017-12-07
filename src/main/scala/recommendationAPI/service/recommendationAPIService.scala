441fd82a3a322f898df4908e9987a9abf865ee65
Shakrah Yves <7shakrahs@gmail.com>
2017-12-07 17:54:40 +0100

    add comments for review

--- src/main/scala/recommendationAPI/service/recommendationAPIService.scala
+++ src/main/scala/recommendationAPI/service/recommendationAPIService.scala
@@ -4,12 +4,19 @@ import scala.io.Source._
 object RecommendationService extends App {
   import RecommendationUtils._
 
+  //Note: this is a command line program so match on the supplied arguments.
   args match {
     case Array(sku, filePath, numRecords_) =>
       val file = scala.io.Source.fromFile(filePath)
       val str = file.mkString
       file.close
       val numRecords = toInt(numRecords_).getOrElse(0)
+
+      //Note: try to decode json yeild err on failure, 
+      //on success try looking up sku supplie as an arg.
+      //If the db exists then proceed to score db entries based on provided Sku.
+      //and return the number or requested results.
+      //else yeild error.
 
       decode(str).flatMap{ db => score(Sku(sku), db)} match {
         case Left(err)   => println(err)
--- src/main/scala/recommendationAPI/service/recommendationAPIUtils.scala
+++ src/main/scala/recommendationAPI/service/recommendationAPIUtils.scala
@@ -16,7 +16,7 @@ object RecommendationUtils {
       ("attributes", r.attrs.map { case (k, v) => k.value -> v.value }.asJson) ->:
       jEmptyObject
     )
-    
+
   implicit def SkuEncodeJson: EncodeJson[Sku] =
     EncodeJson((s: Sku) => s.value.asJson)
 
@@ -34,8 +34,14 @@ object RecommendationUtils {
     vecJson.asJson
   }
 
+  //Note: Perhaps if there were db class and instance of db. This function would be a method on it.
+  //OO Refactor ideas: Then I would not need to pass the db around. However I only do later transformations conditionally,
+  // based on the type of db being Some/None. This could be replaced if there was an empty db type?
+
   def decode(str: String): Either[String, Map[Sku, Map[AttrKey, AttrVal]]] = {
     Parse.decodeEither[Map[String, Map[String, String]]](str).map{skus =>
+      //Note: transform Map to have typed values, because strings less explicit.
+      //If this was a method it might be 2 seperate functions.
       skus.foldLeft(Map[Sku, Map[AttrKey, AttrVal]]()){ (z, pair) =>
         val (sku, attrs) = pair
         z + (Sku(sku) -> attrs.map{ case (k, v) =>  AttrKey(k) -> AttrVal(v)})
@@ -43,27 +49,53 @@ object RecommendationUtils {
     }
   }
   //This funtion is safe because it relies on the current shape of the data having prefixes
-  // and cross collumn matches are not possible.
+  // and cross collumn matches are not possible?
+
+  //Note:
+  //If the db exists then proceed to score db entries based on provided Sku.
+  //and return the number or requested results.
+  //else yeild error.
+
+  //Note: FP Maybe I should have created a type alias for Vector[(sku, Score..)], OO: a class 'Item'
+  //OO Refactor: Passing around tuples, and unpacking and naming them can be cumbersome.
+  //If I used a class then the fileds would already be named and easy to access.
   def score(sku: Sku, skuDb: Map[Sku, Map[AttrKey, AttrVal]]): Either[String, Vector[(Sku, Score, Map[AttrKey, AttrVal])]] = {
     skuDb.get(sku) match {
       case Some(pastMap) =>
+        //Note: get the attribute values for the past purchase for comparison. So that I can use zip in my sorting algorithm.
         val pastAttrVals =  pastMap.toList.sortBy(t => t._1.value).map(_._2)
 
+        //Now I'm buiding up a collection with a more complex data type.
+        // The first item in the tuple is a pair values I will sortby: Score &
+        // matchString: that represents whether a match occured between the past purchased Item attribute
+        //and the possible match we are interating over, by leveraging builtin ordering on strings.
+        //Note: The item's map is passed around so that it can be accessed for printing later.
         val scored_ = skuDb.foldLeft( Vector[ ( (Int, String), (Sku, Score, Map[AttrKey, AttrVal]) ) ]() ) { (z, skuTup) =>
+          //Note: since we are iterating over a map, the key, value pair is unpacked as a tuple.
           val (sku_, attrMap)  = skuTup
+            //Note: If we find the past Purchased sku then we don't include it in mathches.
             if (sku_ == sku){
               z
             } else {
+              //Note: get the attributes that we want to compare as a list, no zip on Maps,
+              //default Maps are not ordered so after getting a tuple of k,v we sort on the keys, then collect the values.
               val attrVals = attrMap.toList.sortBy(t => t._1.value).map(_._2)
+              //Note: create pairs for comparison, filter for matches gives a list of booleans.
               val matches = pastAttrVals.zip(attrVals).map { tup => tup._1 == tup._2 }
+              //Note: the count is equal to score
               val score = matches.filter(x => x).length
+              //Note: Hacky way to use builtin ordering to settle order when scores are equal.
+              //If elem is true 'a' else 'b', seemed clearer than 01, and true false cannot be sorted by normal ordering.
               val matchString = matches.foldLeft(""){(z,a)=> z + (if (a) "a" else "b") }
+              //Normal ordering word sort ascending so we negate the value.
               val comparator = (-score, matchString)
 
               z :+ ( comparator, (sku_, Score(score), attrMap) )
             }
+            //sort by the comparators and the pass the rest of the sku info on.
         }.sortBy{ _._1 }
 
+        //we only pass on the sku's info, not the comparator
         Right(scored_.map{ _._2 })
       case None => Left("The Sku provided was not found")
     }
